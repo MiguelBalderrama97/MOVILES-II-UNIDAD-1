2e22c7c08d5fc09714095d0de80e976c1af4f0ae
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-08-26 12:51:20 -0600

    P4 a medias

--- EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/MainActivity.java
+++ EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.miguel.eva1_4_frag_din;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnFirst, btnSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFirst = findViewById(R.id.btnFisrt);
+        btnSecond = findViewById(R.id.btnSecond);
+
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
