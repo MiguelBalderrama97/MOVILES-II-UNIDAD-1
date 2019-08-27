6133e5916c16bdebbee57dbf70bde9d660ab1093
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-08-27 12:49:53 -0600

    Terminar P4

--- EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/FirstFragment.java
+++ EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/FirstFragment.java
@@ -1,11 +1,14 @@
 package com.example.miguel.eva1_4_frag_din;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -13,15 +16,33 @@ import android.view.ViewGroup;
  */
 public class FirstFragment extends Fragment {
 
+    private Context context;
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        Toast.makeText(context, "onAttachF", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(context, "onCreateF", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(context, "onPauseF", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
--- EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/MainActivity.java
+++ EVA1_4_FRAG_DIN/app/src/main/java/com/example/miguel/eva1_4_frag_din/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.miguel.eva1_4_frag_din;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,18 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnFirst, btnSecond;
 
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    private FirstFragment firstFragment;
+    private SecondFragment secondFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
 
         btnFirst = findViewById(R.id.btnFisrt);
         btnSecond = findViewById(R.id.btnSecond);
@@ -20,14 +30,28 @@ public class MainActivity extends AppCompatActivity {
         btnFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                fragmentTransaction = fragmentManager.beginTransaction();
+                firstFragment = new FirstFragment();
+//                fragmentTransaction.replace(R.id.frameFragmentos, firstFragment);
+//                fragmentTransaction.commit();
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.add(R.id.frameFragmentos,firstFragment,"FIRST");
+                fragmentTransaction.commit();
             }
         });
 
         btnSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                fragmentTransaction = fragmentManager.beginTransaction();
+                secondFragment = new SecondFragment();
+//                fragmentTransaction.replace(R.id.frameFragmentos, secondFragment);
+//                fragmentTransaction.commit();
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.add(R.id.frameFragmentos,secondFragment,"FIRST");
+                fragmentTransaction.commit();
             }
         });
     }
